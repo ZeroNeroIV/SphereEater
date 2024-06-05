6eafdca5fafc714e806abef4eb23a88673e7737c
Alameen Sabbah <70842282+ZeroNeroIV@users.noreply.github.com>
2024-06-05 21:45:50 +0300

    Update GameM.cs

--- Assets/Scripts/GameM.cs
+++ Assets/Scripts/GameM.cs
@@ -7,23 +7,12 @@ public class GameM : MonoBehaviour
     public static GameM instance;
     private int m_score = 0;
     private TMPro.TextMeshProUGUI Text;  
-    void Awake() {
-        if (instance == null) {
+    private void Awake() {
+        if (instance == null)
             instance = this;
-        }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        Text = GameObject.Find("Canvas/ScoreText").GetComponent<TMPro.TextMeshProUGUI>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    private void Start() => Text = GameObject.Find("Canvas/ScoreText").GetComponent<TMPro.TextMeshProUGUI>();
 
     public void AddScore(int score)
     {
